076edd1fbc0d5b5c8e6f8ed6a81ea9359b50a990
Sandy Dang <sandy.dang371@gmail.com>
2023-06-27 10:46:05 -0600

    Initial Commit

---
+++ ToDoApp (day 6)/ToDoApp__day_6_App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp__day_6_App.swift
+//  ToDoApp (day 6)
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp__day_6_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
